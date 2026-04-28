cbc64414c4e55e244d0f7124c85e5359fbf2ccb4
xinjielau <lxjie0776@gmail.com>
2026-04-28 20:13:47 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// JavaScript code to change text when the button is clicked
-document.getElementById('changeTextButton').addEventListener('click', function() {
-    document.querySelector('p').textContent = "You clicked the button! Welcome to my simple website!";
-});
