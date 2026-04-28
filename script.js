8e2c3291f582932ecaf7fd8ec72ba58549632a89
xinjielau <lxjie0776@gmail.com>
2026-04-28 19:51:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript code to change text when the button is clicked
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.querySelector('p').textContent = "You clicked the button! Welcome to my simple website!";
+});
