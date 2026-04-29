bd0f6a8ab92b5b07b4c7bd8e88d9c6036059f879
xinjielau <lxjie0776@gmail.com>
2026-04-29 09:28:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// Button Click Action for Join the Movement
+document.getElementById("ctaButton").addEventListener("click", function() {
+    window.location.href = "https://shepower.com/join";  // Redirects to the signup page
+});
